f30842bb06dc013eaac7d9071a9e8d0bbe40d3e7
krable91 <dydansp@naver.com>
2019-12-31 05:25:47 +0900

    Server Setup

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {join} from "path";
+import socketIO from "socket.io";
+
+const app = express();
+const PORT = 4000;
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>console.log(`server runnings: https://localhost:${PORT}`);
+app.listen(PORT, handleListening);
